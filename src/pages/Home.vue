aa666f013e117937cb43610e3b29919e95c906fe
Anthony <blacknebula236@gmail.com>
2025-09-20 20:59:14 +0930

    add hero to home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,17 @@
 <template>
-    <h1>This is the home page</h1>
+    <div class="container mx-auto px-4 py-8">
+        <section class="hero bg-base-200 p-20 rounded-2xl">
+            <div class="hero-content text-center">
+                <div class="max-w-md">
+                    <h1 class="text-5xl font-bold">Web Tools</h1>
+                    <p class="py-6">
+                        Web tools that work in the browser. Free, fast and open source.
+                    </p>
+                    <button class="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script setup lang="ts">
