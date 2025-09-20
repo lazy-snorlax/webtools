1ede36f82523bba37b58acfaf2bab6b82de119ab
Anthony <blacknebula236@gmail.com>
2025-09-20 20:58:22 +0930

    add sidebar component

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="p-4">
+        <div class="mb-6">
+            <div class="space-y-1">
+                <router-link :to="'/json-beautify'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">JSON Beautify</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
