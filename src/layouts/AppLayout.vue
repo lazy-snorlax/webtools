2c0e032981dd27c5bee8d1a8409a08be4b1a7358
Anthony <blacknebula236@gmail.com>
2025-09-20 20:58:47 +0930

    create app layout

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -4,11 +4,11 @@
             <div class="container mx-auto px-4">
                 <div class="flex items-center justify-between h-16">
                     <div class="flex items-center gap-4">
-                        <a class="flex items-center gap-3" href="/">
+                        <router-link class="flex items-center gap-3" :to="'/'">
                             <div class="hidden sm:block">
                                 <span class="text-xl font-bold text-white">WebTools</span>
                             </div>
-                        </a>
+                        </router-link>
                     </div>
                     <div class="flex items-center gap-4">
                         <a class="hidden md:inline-flex items-center px-3 py-2 text-zinc-300 rounded-lg font-medium hover:bg-zinc-800 transition-colors" href="/about">About</a>
@@ -21,11 +21,13 @@
         
         <div class="flex">
             <aside class="fixed left-0 top-16 w-64 border-r h-[calc(100vh-4rem)]">
-                <p>This is a sidebar</p>
+                <sidebar />
             </aside>
             <main class="flex-1 transition-all duration-300 lg:ml-64">
                 <div class="min-h-[calc(100vh-4rem)] pt-16">
-                    <slot />
+                    <div class="container mx-auto px-4 py-8">
+                        <slot />
+                    </div>
                 </div>
                 <footer class="border-t ">
                     <p>This is a footer</p>
@@ -36,5 +38,5 @@
 </template>
 
 <script setup lang="ts">
-
+import Sidebar from '@/components/Sidebar.vue';
 </script>
