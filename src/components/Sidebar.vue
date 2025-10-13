cef624849e05a2607b5a098369ab241194a7576d
Anthony <blacknebula236@gmail.com>
2025-10-13 22:29:34 +1030

    add xml formatter to nav

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -7,6 +7,7 @@
                 <router-link :to="'/uuid-generator'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">UUID Generator</router-link>
                 <router-link :to="'/lorem-generator'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">Lorem Ipsum Generator</router-link>
                 <router-link :to="'/json-formatter'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">JSON Formatter</router-link>
+                <router-link :to="'/xml-formatter'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">XML Formatter</router-link>
                 <router-link :to="'/json-converter'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">JSON Converter</router-link>
             </div>
         </div>
