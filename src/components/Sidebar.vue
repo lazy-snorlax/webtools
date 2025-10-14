cb7d407dd56260d7275091d0b6a2db4e420432f5
Anthony <blacknebula236@gmail.com>
2025-10-14 22:20:57 +1030

    add new nav link

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -6,9 +6,12 @@
                 <router-link :to="'/timestamp-converter'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">Timestamp Converter</router-link>
                 <router-link :to="'/uuid-generator'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">UUID Generator</router-link>
                 <router-link :to="'/lorem-generator'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">Lorem Ipsum Generator</router-link>
+                
                 <router-link :to="'/json-formatter'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">JSON Formatter</router-link>
                 <router-link :to="'/xml-formatter'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">XML Formatter</router-link>
+                
                 <router-link :to="'/json-converter'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">JSON Converter</router-link>
+                <router-link :to="'/xml-converter'" class="group flex items-center gap-3 rounded-lc px-3 py-2 text-sm btn btn-ghost">XML Converter</router-link>
             </div>
         </div>
     </div>
